c05b0f7066ae9c8e73d43c0a51c558413f0e3637
Igor <redsadeen@gmail.com>
2021-10-19 19:00:47 +0200

    Adding UI

---
+++ app/src/main/java/com/example/chatv3/MessageChat.kt
@@ -0,0 +1,20 @@
+package com.example.chatv3
+
+import java.awt.font.TextAttribute
+
+class MessageChat(
+    var text: String,
+    var name: String,
+    var imageUrl: String
+) {
+
+////    lateinit var text: String
+////    lateinit var name: String
+//    private lateinit var iageUrl: String
+//
+//
+//    override fun toString(): String {
+//        return "MessageChat(text='$text', name='$name', imageUrl='$imageUrl', iageUrl='$iageUrl')"
+    }
+
+
