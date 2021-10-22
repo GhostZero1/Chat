7da31b2a449ce42376a9be8fae1f4420f7914a30
Igor <redsadeen@gmail.com>
2021-10-22 19:31:23 +0200

    Added Sign Up code

--- app/src/main/java/com/example/chatv3/SingInActivity.kt
+++ app/src/main/java/com/example/chatv3/SingInActivity.kt
@@ -1,11 +1,65 @@
 package com.example.chatv3
 
+import android.app.Activity
+import android.content.ContentValues.TAG
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.EditText
+import android.widget.Toast
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.activity_sing_in.*
 
 class SingInActivity : AppCompatActivity() {
+
+
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sing_in)
+        var emailEditText = findViewById<EditText>(R.id.emailEditText)
+        var passwordEditText = findViewById<EditText>(R.id.passwordEditText)
+        var nameEditText = findViewById<EditText>(R.id.nameEditText)
+        super.onCreate(savedInstanceState)
+        auth = Firebase.auth
+
+        signUpLogInButton.setOnClickListener {
+            singUpUser(email = emailEditText.text.toString().trim(),password = passwordEditText.text.toString().trim())
+        }
     }
-}
+
+        private fun singUpUser(email: String, password: String) {
+            auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this@SingInActivity) { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Log.d("Sign", "createUserWithEmail:success")
+                        val user = auth.currentUser
+                        //updateUI(user)
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.w("Sign", "createUserWithEmail:failure", task.exception)
+                        Toast.makeText(
+                            this@SingInActivity, "Authentication failed.",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                        //updateUI(null)
+                    }
+                }
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
